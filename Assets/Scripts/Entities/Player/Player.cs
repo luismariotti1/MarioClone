50a878f6e33505b0ef4833b4bf2a6e7f54232a0a
Luis Mariotti <fernando-mariotti@outlook.com>
2021-06-19 21:54:45 -0300

    construtor

--- Assets/Scripts/Entities/Player/Player.cs
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -1,4 +1,4 @@
-using Helper;
+using System;
 using UnityEngine;
 
 namespace Entities.Player
@@ -16,6 +16,12 @@ namespace Entities.Player
         [SerializeField]
         private PlayerStats stats;
 
+        public Player()
+        {
+            stats.movementSpeed = 1;
+            stats.jumpForce = 3.25f;
+            stats.maxJumpForce = 4.25f;
+        }
         public PlayerStats getStats()
         {
             return stats;
